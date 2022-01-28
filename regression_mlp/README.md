9d93eabfe91f8b8f0c6d396aa3f72d6f90937c60
eusebioaguilera <eusebio.aguilera@gmail.com>
2022-01-28 12:03:00 +0100

    Regression example with sequential and no sequential models

---
+++ regression_mlp/README.md
@@ -0,0 +1,4 @@
+# MLP for regression
+
+This directory contains a MLP model used for solving a regression problem.
+The model is used to solve the well known california house price regression problem.
---
+++ regression_mlp/train_mlp.py
@@ -0,0 +1,108 @@
+from tkinter.messagebox import NO
+from matplotlib.pyplot import sca
+from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+import keras.models
+import keras.layers
+from keras.losses import mean_squared_error
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def get_sequencial_model():
+    """
+        This method builds a sequential model based on MLP to solve the regression problem
+    """
+    model = keras.models.Sequential(
+        [
+            # Hidden layer with 30 neurons and the relu activation function
+            keras.layers.Dense(30, activation="relu"),
+            # Output layer with one neuron (the value predicted) and no activation function
+            keras.layers.Dense(1)
+        ]
+    )
+
+    return model
+
+
+def get_neural_mlp_model(input_shape, activation_func='relu'):
+    """
+        This method implements a model with a non-sequential topology. For this purpose we need to
+        use the Functional API provided by Keras. 
+        The topology simply concatenate the input to the final hidden layer in order to add information in the final step
+    """
+    input = keras.layers.Input(shape=input_shape)
+    hidden_layer_1 = keras.layers.Dense(
+        units=30, activation=activation_func)(input)
+    hidden_layer_2 = keras.layers.Dense(
+        units=100, activation=activation_func)(hidden_layer_1)
+    hidden_layer_3 = keras.layers.Dense(
+        units=30, activation=activation_func)(hidden_layer_2)
+    concat_layer = keras.layers.Concatenate()([input, hidden_layer_3])
+    output_layer = keras.layers.Dense(1)(concat_layer)
+    # Create the model defining the input layers and the output ones
+    model = keras.models.Model(inputs=[input], outputs=[output_layer])
+
+    return model
+
+
+# Obtain the dataset
+dataset = fetch_california_housing()
+print(dataset.keys())
+
+# Split the dataset between train/validation/test
+
+X_train, X_test, y_train, y_test = train_test_split(
+    dataset.data, dataset.target, train_size=0.9)
+
+X_train, X_valid, y_train, y_valid = train_test_split(
+    X_train, y_train, train_size=0.9)
+
+# Normalize the dataset
+scaler = MinMaxScaler()
+# The normalization fit must be done only on the train subset
+X_train = scaler.fit_transform(X_train)
+# Then we transform (normalize) on the rest of the subsets
+X_test = scaler.transform(X_test)
+X_valid = scaler.transform(X_valid)
+
+
+seq_model = get_sequencial_model()
+
+seq_model.compile(optimizer='adam', loss=mean_squared_error)
+
+# Model training
+seq_history = seq_model.fit(x=X_train, y=y_train, batch_size=100,
+                            epochs=50, validation_data=(X_valid, y_valid))
+
+# Model evaluation
+mse = seq_model.evaluate(x=X_test, y=y_test)
+
+print("Sequential model MSE: {}".format(mse))
+
+# Create non sequential model
+no_seq_model = get_neural_mlp_model(X_train.shape[1:])
+no_seq_model.compile(optimizer='adam', loss=mean_squared_error)
+
+# Model trainig
+no_seq_history = no_seq_model.fit(x=X_train, y=y_train, batch_size=100,
+                                  epochs=50, validation_data=(X_valid, y_valid))
+
+# Model evaluation
+no_seq_mse = no_seq_model.evaluate(x=X_test, y=y_test)
+
+print("No Sequential model MSE: {}".format(no_seq_mse))
+
+
+# Plot and save learning curves
+pd.DataFrame(seq_history.history).plot(figsize=(8, 5))
+plt.grid(True)
+plt.gca().set_ylim(0, 1)  # set the vertical range to [0-1]
+plt.savefig("seq_learning_history.png")
+
+# Plot and save learning curves
+pd.DataFrame(no_seq_history.history).plot(figsize=(8, 5))
+plt.grid(True)
+plt.gca().set_ylim(0, 1)  # set the vertical range to [0-1]
+plt.savefig("no_seq_learning_history.png")
